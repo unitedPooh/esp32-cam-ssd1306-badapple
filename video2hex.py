8e2f6626054bf32f4aa565691fcfd726668bb68c
united_pooh <100600801+unitedPooh@users.noreply.github.com>
2024-12-20 16:01:30 +0800

    Update video2hex.py

--- video2hex.py
+++ video2hex.py
@@ -10,20 +10,20 @@ def Video2Hex(
     if not cap.isOpened():
         print("Error: Could not open video.")
         exit()
-    ret, frame = cap.read()
-    if not ret:
-        print("End of video or error occurred.")
-        exit()
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     try:
+        # index = 0
         with open(filename, "ab") as f:
             for _ in tqdm(range(total_frames)):
+                ret, frame = cap.read()
+                if not ret:
+                    print("End of video or error occurred.")
+                    exit()
                 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
                 resized = cv2.resize(
                     gray, (width, height), interpolation=cv2.INTER_AREA
                 )
-
                 image = Image.fromarray(resized)
                 hres = b""
                 for k in range(height // 8):
@@ -31,12 +31,11 @@ def Video2Hex(
                         res = ""
                         for y in range(8):
                             pixel = image.getpixel((x, 8 * k + y))
-                            # 将像素值转换为1或0
-                            res = "1" + res if pixel > 128 else "0" + res
-                        temp = "{:02x}".format(int(res, 2))
+                            res += "1" if pixel > 128 else "0"
+                        temp = "{:02x}".format(int(res[::-1], 2))
                         hres += bytes.fromhex(temp)
-                    f.write(hres)
-                    f.write(b"\n")
+                f.write(hres)
+                f.write(b"\n")
     except Exception as e:
         print(f"Error: {e}")
         exit()
@@ -47,5 +46,5 @@ def Video2Hex(
 
 if __name__ == "__main__":
     video_name = "badapple.mp4"
-    output_filename = "output.hex"
+    output_filename = "badapple.hex"
     Video2Hex(video_name, output_filename)
